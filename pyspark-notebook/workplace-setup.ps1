803b1a4f53739ca54b20b2d024484a3cfcb276cf
simicd <10134699+simicd@users.noreply.github.com>
2020-08-02 14:16:30 +0200

    Update dockerfile to Ubuntu 20.04 and Spark 3.0 incl. dependencies

--- pyspark-notebook/workplace-setup.ps1
+++ pyspark-notebook/workplace-setup.ps1
@@ -1,26 +1,26 @@
 # Install additional Python packages
-pip install pyarrow==0.17.0
-pip install koalas==0.32.0
-pip install pandas==0.25.3
-pip install streamlit==0.59.0
-pip install pytest-cov==2.8.1
-pip install sphinx==3.0.3
-pip install sphinx-autodoc-typehints==1.10.3
-pip install sphinx_rtd_theme==0.4.3
-pip install pydata-sphinx-theme==0.2.2
+pip install pyarrow==1.0.0
+pip install koalas==1.1.0
+pip install pandas==1.1.0
+pip install streamlit==0.64.0
+pip install pytest-cov==2.10.0
+pip install sphinx==3.1.2
+pip install sphinx-autodoc-typehints==1.11.0
+pip install sphinx-rtd-theme==0.5.0
+pip install pydata-sphinx-theme==0.3.1
 pip install requests-kerberos==0.12.0
 pip install yapf==0.30.0
 pip install pydot==1.4.1
-pip install graphviz==0.14
+pip install graphviz==0.14.1
 pip install cookiecutter==1.7.2
-pip install azure-functions==1.2.1
+pip install azure-functions==1.3.0
 
 # Install JupyterLab
-pip install jupyterlab==2.1.2
+pip install jupyterlab==2.2.2
 npm install -g jupyterlab-tailwind-theme
 
 # Install and activate Jupyter extensions
-pip install jupyter_contrib_nbextensions==0.5.1
+pip install jupyter-contrib-nbextensions==0.5.1
 jupyter contrib nbextension install --user
 jupyter nbextensions_configurator enable --user
 pip install rise==5.6.1
@@ -41,7 +41,7 @@ Invoke-WebRequest -Uri https://gist.githubusercontent.com/simicd/20d40cae61305ae
 # Add VS Code settings file
 mkdir $env:USERPROFILE/AppData/Roaming/Code/User
 cd $env:USERPROFILE/AppData/Roaming/Code/User
-Invoke-WebRequest -Uri https://gist.githubusercontent.com/simicd/7bb64dad99699451264b983e6a671eac/raw/2e3db5ecd9a53257972d233a6ab5e544c8b44db3/settings.json -OutFile "settings.json"
+Invoke-WebRequest -Uri https://gist.githubusercontent.com/simicd/7bb64dad99699451264b983e6a671eac/raw/5e992e14f9e20a774029c5464e8ed12583b498d8/settings.json -OutFile "settings.json"
 
 # Change back to user folder
 cd $env:USERPROFILE
